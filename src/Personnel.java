be155d35a09021a4b9b3e032eda3212f54a4e389
eloiselenoir78@gmail.com <eloiselenoir78@gmail.com>
2025-01-05 22:18:29 +0100

    Third commit

--- src/Personnel.java
+++ src/Personnel.java
@@ -1,16 +1,41 @@
-public class Personnel extends Employe {
-    private String historiquePersonnel;
-    public Personnel(String historiquePersonnel) {
-        super();
-        this.historiquePersonnel = historiquePersonnel;
+public class Personnel {
+    private int numeroemploye;
+    private String dateEmbauche;
+    private String nomEmploye;
+    private String historiqueEmploye;
+
+    public Personnel(String dateEmbauche, int numeroemploye, String nomEmploye, String historiqueEmploye) {
+        this.dateEmbauche = dateEmbauche;
+        this.numeroemploye = numeroemploye;
+        this.nomEmploye = nomEmploye;
+        this.historiqueEmploye = historiqueEmploye;
     }
 
-    public String getHistoriquePersonnel() {
-        return historiquePersonnel;
+    public int getNumeroemploye() {
+        return numeroemploye;
+    }
+    public String getDateEmbauche() {
+        return dateEmbauche;
+    }
+    public String getNomEmploye() {
+        return nomEmploye;
+    }
+    public String getHistoriqueEmploye() {
+        return historiqueEmploye;
     }
 
     @Override
     public String toString() {
-        return "Personnel{" + "historiquePersonnel='" + historiquePersonnel + '\'' + ", nomEmploye='" + nomEmploye + '\'' +'}';
+        return "Employe{" +
+                "numeroemploye=" + numeroemploye +
+                ", dateEmbauche='" + dateEmbauche + '\'' +
+                ", nomEmploye='" + nomEmploye + '\'' +
+                ", historiqueEmploye='" + historiqueEmploye + '\'' +
+                '}';
+    }
+
+    //Fonction ObtenirRole()
+    public void ObtenirRole() {
+        System.out.println("Role = void");
     }
 }
