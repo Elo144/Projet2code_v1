266895ef4bf28faa40ba5edc110fe64f9c7fa84d
eloiselenoir78@gmail.com <eloiselenoir78@gmail.com>
2025-01-06 10:07:58 +0100

    4e commit

--- src/Personnel.java
+++ src/Personnel.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Personnel {
     private int numeroemploye;
     private String dateEmbauche;
@@ -24,18 +27,62 @@ public class Personnel {
         return historiqueEmploye;
     }
 
+    //    //Fonction ObtenirRole()
+//    public void ObtenirRole() {
+//        System.out.println("Role = void");
+//    }
+
+    private List<Personnel> employes = new ArrayList<>();
+    //CREER
+    public void ajouterUnEmploye(Personnel employe) {
+        employes.add(employe);
+        System.out.println("Tache ajoutée : " + employe);
+    }
+
+    //MODIFIER
+    public void modifierPersonnel(String dateEmbauche, int numeroemploye, String nomEmploye, String historiqueEmploye) {
+        for (Personnel employe : employes) {
+            if (employe.getNumeroemploye() == numeroemploye) {
+                employe.nomEmploye = nomEmploye;
+                employe.dateEmbauche = dateEmbauche;
+                employe.historiqueEmploye = historiqueEmploye;
+                System.out.println("Personnel modifié : " + employe);
+            }
+        }
+        System.out.println("Personnel " + nomEmploye + " pas trouvé");
+    }
+
+    //SUPPRIMER
+    public void supprimerPersonnel(int numeroemploye) {
+        for (Personnel employe : employes) {
+            if (employe.getNumeroemploye() == numeroemploye) {
+                employes.remove(employe);
+                System.out.println("Employe supprimé : "+ numeroemploye);
+                return;
+            }
+        }
+    }
+
+    //VISUALISER INFOS PERSONNEL
     @Override
     public String toString() {
-        return "Employe{" +
+        return "Personnel{" +
                 "numeroemploye=" + numeroemploye +
                 ", dateEmbauche='" + dateEmbauche + '\'' +
                 ", nomEmploye='" + nomEmploye + '\'' +
                 ", historiqueEmploye='" + historiqueEmploye + '\'' +
+                ", employes=" + employes +
                 '}';
     }
 
-    //Fonction ObtenirRole()
-    public void ObtenirRole() {
-        System.out.println("Role = void");
+    //AFFICHER HISTORIQUE
+    public void afficherHistorique(int numeroemploye) {
+        for (Personnel employe : employes) {
+            if (employe.getNumeroemploye() == numeroemploye) {
+                String historique = employe.getHistoriqueEmploye();
+                System.out.println("Voici l'historique de l'employe avec l'id " + nomEmploye + " : " + historique);
+                return;
+            }
+        }
     }
 }
